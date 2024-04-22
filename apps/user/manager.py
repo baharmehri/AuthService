160c124656e6633f8c7687f6f42155332423ed3e
Bahar Mehri <b.mehri@lifeweb.ir>
2024-04-22 12:17:47 +0330

    add custom user

---
+++ apps/user/manager.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, is_active=True, is_admin=False, **kwargs):
+        if not email:
+            raise ValueError("Users must have an email")
+        if not password:
+            raise ValueError("Users must have an password")
+
+        user = self.model(is_active=is_active, email=email, is_admin=is_admin, **kwargs)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **kwargs):
+        kwargs.setdefault('is_staff', True)
+        kwargs.setdefault('is_superuser', True)
+        kwargs.setdefault('is_active', True)
+
+        if kwargs.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if kwargs.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **kwargs)
--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from .manager import CustomUserManager
+
 
 # Create your models here.
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    is_admin = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email'
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
